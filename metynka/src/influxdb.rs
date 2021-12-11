72523450358774530740d8d620f192e76909bf29
prace@srbpavel.cz <prace@srbpavel.cz>
2021-12-11 22:22:32 +0100

    fn data line to vec

--- metynka/src/influxdb.rs
+++ metynka/src/influxdb.rs
@@ -1,3 +1,5 @@
+//use core::option::IterMut;
+
 use std::process::{Command};
 use std::{thread, time};
 use std::collections::HashMap;
@@ -110,6 +112,18 @@ fn yield_flux_result_records(records: Vec<HashMap<String, String>>,
 }
 
 
+fn flux_responde_data_line_to_vec(lines: Option<String>) -> Vec<String> {
+    lines
+        .unwrap_or_else(|| {
+            eprintln!("\nERROR: in FLUX RESPONDE DATA line");
+            "error".to_string()
+        })
+        .split(',')
+        .map(|k| k.to_string())
+        .collect::<Vec<String>>()
+}
+
+
 fn flux_csv_to_hash(config: &TomlConfig,
                     data: String) -> Vec<HashMap<String, String>> {
 
@@ -118,36 +132,23 @@ fn flux_csv_to_hash(config: &TomlConfig,
     let mut values: Vec<Vec<String>> = Vec::new();
     let mut records: Vec<HashMap<String, String>> = Vec::new();
 
-    //let lines_count = data.lines().count();
-    
-    //let mut lines = data.lines();
+    // filter empty lines
+    //let mut lines = data
     let mut lines = data
         .lines()
-        .filter(|line| {
-            /*
-            let l = line.trim();
-            let status = !l.is_empty();
-            
-            println!("l[{}]: {:#?}", status, l);
-            
-            //l != ""
-            status
-            */
-            // !line.is_empty()
-            !line.trim().is_empty()
-        })
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| line.to_string()) // fn
         .collect::<Vec<_>>()
         .into_iter();
 
-    // last empty line not skiped and rather verified for valid data
-    //for i in 1..=lines_count {
-    for i in 1..=lines.len()+2 {
+    for i in 1..=lines.len() {
         match i {
             // FIRST line to Vec<keys>
             1 => {
+                keys = flux_responde_data_line_to_vec(lines.next());
+                /*
                 keys = lines
                     .next()
-                    //.unwrap()
                     .unwrap_or_else(|| {
                         eprintln!("\nERROR: in FLUX RESPONDE DATA line");
                         "ERROR"
@@ -155,47 +156,35 @@ fn flux_csv_to_hash(config: &TomlConfig,
                     .split(',')
                     .map(|k| k.to_string())
                     .collect::<Vec<String>>();
+                */
 
-                //println!("keys: {:?}", keys);
-                
                 keys_len = keys.len();
             },
             // OTHER lines APPEND to Vec<values> if valid
             _ => {
+                let items = flux_responde_data_line_to_vec(lines.next());
+                /*
                 let items = lines
                     .next()
-                    //.unwrap()
-                    // /*
                     .unwrap_or_else(|| {
                         eprintln!("\nERROR: in FLUX RESPONDE DATA line");
                         "ERROR"
                     })
-                    // */
                     .split(',')
                     .map(|v| v.to_string())
                     .collect::<Vec<String>>();
-
-                //println!("values: {:?}", items);
+                */
 
                 if items.len() == keys_len {
                     values.push(items)
                 }
                 else { 
                     if config.flag.debug_flux_result_invalid_line {
-                        let f = format!("\nWARNING: in FLUX RESPONDE DATA line >>> values does not fit keys: values_len: {vl} / keys_len: {kl}\nVALUES: {:?}",
-                                        i=items,
-                                        vl=items.len(),
-                                        kl=keys_len,
-                        );
-
-                        eprintln!("{}", f);
-                        /*
                         eprintln!("\nWARNING: in FLUX RESPONDE DATA line >>> values does not fit keys: values_len: {vl} / keys_len: {kl}\nVALUES: {:?}",
                                   i=items,
                                   vl=items.len(),
                                   kl=keys_len,
                         );
-                        */
                     }
                 }
             }
