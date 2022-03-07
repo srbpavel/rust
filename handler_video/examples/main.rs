101ab6d60d9b02460153c1dcae2875c1c4d7ca4d
prace@srbpavel.cz <prace@srbpavel.cz>
2022-03-07 22:13:27 +0100

    async-process filler works

--- handler_video/examples/main.rs
+++ handler_video/examples/main.rs
@@ -6,10 +6,7 @@ mod example_fill_toml_config_struct;
 mod util;
 
 use template_formater::tuple_formater;
-use std::io::prelude::*;
-
-//use async_std::stream::StreamExt;
-use async_std::prelude::*;
+use std::io::Write;
 
 use example_fill_toml_config_struct::TomlConfig;
 
@@ -25,38 +22,10 @@ struct Video {
 
 /// single curl video upload
 async fn run_upload(config: &TomlConfig,
-                    video: Video) -> Option<String> {
+                    //video: Video) -> Result<String, std::io::Error> {
+                    video: Video) -> Result<String, Box<dyn std::error::Error>> {
 
-    let mut cmd = std::process::Command::new("curl");
-    
-    cmd.arg("-X")
-        .arg("PUT")
-        .arg("-H")
-        .arg("Content-type: multipart/form-data")
-        .arg(video.upload_url)
-        .arg("-F")
-    // type hardcoded as all mp4
-        .arg(
-            &format!("{name}=@{filename};type=video/mp4",
-                     name = video.name,
-                     filename = video.filename,
-            )
-        )
-        .arg("-H")
-        .arg(
-            &format!("video_id: {}",
-                     video.video_id,
-            )
-        )
-        .arg("-H")
-        .arg("group: youtube");
-    
-    println!("\n\n#CMD: {:?}", cmd);
-    
-    let output = cmd.output();
-    
-    println!("#OUTPUT: {output:#?}");
-
+    // HTML
     let video_path = format!("{}/{}",
                              &video.player_url,
                              video.video_id
@@ -89,10 +58,39 @@ async fn run_upload(config: &TomlConfig,
     ).unwrap();
     
     single_html_file.write_all(
-        &single_html_code.as_bytes()
+        single_html_code.as_bytes()
     ).unwrap();
     
-    Some(video_tag)
+    // UPLOAD
+    let mut cmd = async_process::Command::new("curl");
+    
+    cmd.arg("-X")
+        .arg("PUT")
+        .arg("-H")
+        .arg("Content-type: multipart/form-data")
+        .arg(video.upload_url)
+        .arg("-F")
+        // type hardcoded as all mp4
+        .arg(
+            &format!("{name}=@{filename};type=video/mp4",
+                     name = video.name,
+                     filename = video.filename,
+            )
+        )
+        .arg("-H")
+        .arg(
+            &format!("video_id: {}",
+                     video.video_id,
+            )
+        )
+        .arg("-H")
+        .arg("group: youtube");
+    
+    println!("#CMD: {:?}", cmd);
+    let _output = cmd.output().await?;
+    //println!("#OUTPUT: {output:#?}");
+    println!("#UPLOADED: {}", video.filename);   
+    Ok(video_tag)
 }
 
 
@@ -115,8 +113,6 @@ async fn main() -> std::io::Result<()> {
                              path=config.upload_path,
     );
 
-    println!("UPLOAD_URL: {upload_url}");
-    
     let player_url = format!("{secure}://{server}:{port}{path}",
                              secure=config.secure,
                              server=config.server,
@@ -124,12 +120,12 @@ async fn main() -> std::io::Result<()> {
                              path=config.player_path,
     );
 
+    /*
+    println!("UPLOAD_URL: {upload_url}");
     println!("PLAYER_URL: {player_url}");
-
-    println!("HTML: {}",
-             &config.html_file,
-    );
-        
+    println!("HTML: {}", &config.html_file);
+    */
+  
     // VIDEO_DIR
     let video_dir_path = match std::path::Path::new(&*config.video_dir)
         .canonicalize() {
@@ -156,71 +152,66 @@ async fn main() -> std::io::Result<()> {
     let mut video_files = Vec::new();
 
     // GET READY ALL VIDEO FILES
-    match read_dir {
-        Some(dir) => {
-            for element in dir {
-                match element
-                    .as_ref()
-                    .and_then(|e| Ok(match e.metadata() {
-                        Ok(m) => format!("{}", m.is_file()),
-                        Err(err) => {
-                            eprintln!("ERROR: element METADATA: {:?}\nREASON >>> {}",
-                                      element,
-                                      err,
-                            );
-
-                            String::from("")
-                        },
-                    })) {
-                        Ok(n) => {
-                            match n.as_str() {
-                                // FILE
-                                "true" => {
-                                    match &element {
-                                        Ok(file) => {
-                                            let file_path = file.path();
-                                            
-                                            //println!("FILE_PATH: {file_path:?}");
-
-                                            video_files.push(file_path);
-                                        },
-                                        Err(err) => {
-                                            eprintln!("ERROR: FILE element: {:?}\n REASON >>> {}",
-                                                      element,
-                                                      err,
-                                            )  
-                                        },
-                                    }
+    if let Some(dir) = read_dir {
+        for element in dir {
+            match element
+                .as_ref()
+                .map(|e| match e.metadata() {
+                    Ok(m) => format!("{}", m.is_file()),
+                    Err(err) => {
+                        eprintln!("ERROR: element METADATA: {:?}\nREASON >>> {}",
+                                  element,
+                                  err,
+                        );
+                        
+                        String::from("")
+                    },
+                }) {
+                    Ok(n) => {
+                        // FILE
+                        if n.as_str().eq("true") {
+                            match &element {
+                                Ok(file) => {
+                                    let file_path = file.path();
+                                    
+                                    //println!("FILE_PATH: {file_path:?}");
+                                    
+                                    video_files.push(file_path);
                                 },
-                                // DIR
-                                "false" => {},
-                                _ => {},
+                                Err(err) => {
+                                    eprintln!("ERROR: FILE element: {:?}\n REASON >>> {}",
+                                              element,
+                                              err,
+                                    )  
+                                },
                             }
-                        },
-                        Err(err) => {
-                            eprintln!("ERROR: element: {:?}\nREASON >>> {}",
-                                      element,
-                                      err,
-                            );
-                        },
-                    }
-            }
+                        }
+                    },
+                    Err(err) => {
+                        eprintln!("ERROR: element: {:?}\nREASON >>> {}",
+                                  element,
+                                  err,
+                        );
+                    },
+                }
         }
-        None => {},
-    }
+    };
 
-    println!("FILES: {:?}", video_files);
+
+    /*
+    println!("FILES: {:#?}", video_files);
 
     println!("SAMPLE_LIMIT: {}..{}",
              &config.sample_limit_start,
              &config.sample_limit_end,
     );
-    
+    */
+
     let video_files_sample = match &config.sample_limit_end {
         -1 => video_files,
         n @ 1.. => {
             video_files[
-                (*&config.sample_limit_start as usize)..(*n as usize)
+                (config.sample_limit_start as usize)..(*n as usize)
             ].to_vec()
         },
         _ => {
@@ -232,7 +223,7 @@ async fn main() -> std::io::Result<()> {
         }
     };
     
-    let binary_paths = video_files_sample
+    let binary_paths: Vec<_> = video_files_sample
         .iter()
         .map(|f| {
 
@@ -240,118 +231,36 @@ async fn main() -> std::io::Result<()> {
                 Some(s) => s,
                 None => "",
             };
-
+            
             let name = match &f.file_stem() {
                 Some(n) => n.to_str().unwrap(),
                 None => "",
             };
-
-            let video_id = uuid::Uuid::new_v4();
-
-            /*
-            let mut cmd = std::process::Command::new("curl");
-
-            cmd.arg("-X")
-                .arg("PUT")
-                .arg("-H")
-                .arg("Content-type: multipart/form-data")
-                .arg(&upload_url)
-                .arg("-F")
-                // type hardcoded as all mp4
-                .arg(
-                    &format!("{name}=@{filename};type=video/mp4")
-                )
-                .arg("-H")
-                .arg(
-                    &format!("video_id: {video_id}")
-                )
-                .arg("-H")
-                .arg("group: youtube");
-
-            println!("\n\n#CMD: {:?}", cmd);
-            */
-
-            // EXPENSIVE UPLOAD HERE
             
             let video = Video {
-                name: String::from(name),
-                filename: String::from(filename),
-                player_url: String::from(&player_url),
-                upload_url: String::from(&upload_url),
-                video_id: video_id,
+                name: name.to_string(),
+                filename: filename.to_string(),
+                player_url: player_url.to_string(),
+                upload_url: upload_url.to_string(),
+                video_id: uuid::Uuid::new_v4(),
             };
 
             run_upload(&config,
                        video,
             )
-            
-            /*
-            let output = &cmd.output();
-
-            println!("#OUTPUT: {output:#?}");
-
-            let video_path = format!("{}/{}",
-                                     player_url,
-                                     video_id
-            );
-
-            let video_tag = tuple_formater(
-                &config.video_tag,
-                &vec![
-                    ("name", &name),
-                    ("width", &config.player_width),
-                    ("src", &video_path),
-                ],
-                config.flag.debug_template,
-            );
-
-            let single_html_code = tuple_formater(
-                &config.html_template,
-                &vec![
-                    ("all_videos", &video_tag),
-                ],
-                config.flag.debug_template,
-            );      
-
-            let mut single_html_file = std::fs::File::create(
-                format!("{}{}_{}.html",
-                        &*config.html_path,
-                        &video_id,
-                        &name,
-                )
-            ).unwrap();
-
-            single_html_file.write_all(
-                &single_html_code.as_bytes()
-            ).unwrap();
-            
-            video_tag
-            */
         })
-        .collect::<Vec<_>>();
-        //.concat();
+        .collect();
 
-    let _ = futures::future::join_all(binary_paths).await;
+    let cmd_results = futures::future::join_all(binary_paths).await;
+
+    cmd_results
+        .iter()
+        .for_each(|r|
+                  if r.is_err() {
+                      println!("{r:?}");
+                  }
+        );
     
-    /*
-    let html_code = tuple_formater(
-        &config.html_template,
-        &vec![
-            ("all_videos", &binary_paths),
-        ],
-        config.flag.debug_template,
-    );
-
-    let mut html_file = std::fs::File::create(
-        format!("{}{}",
-                &*config.html_path,
-                &*config.html_file,
-        )
-    )?;
-
-    html_file.write_all(html_code.as_bytes())?;
-    */
-
     Ok(())
 }
 
